edc6e2ba232bf5148f3be074c8f3fd286f560cd8
hunter86rsl <hunter86r@onet.eu>
2018-02-10 12:41:42 +0100

    lecture36

--- kody/OOP-Part-1-Classes-Part-2-Source-code/src/com/timbuchalka/Car.java
+++ kody/OOP-Part-1-Classes-Part-2-Source-code/src/com/timbuchalka/Car.java
@@ -1,6 +1,10 @@
 package com.timbuchalka;
 
+/**
+ * Created by dev on 8/3/15.
+ */
 public class Car {
+
     private int doors;
     private int wheels;
     private String model;
@@ -9,7 +13,7 @@ public class Car {
 
     public void setModel(String model) {
         String validModel = model.toLowerCase();
-        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+        if(validModel.equals("carrera") || validModel.equals("commodore")) {
             this.model = model;
         } else {
             this.model = "Unknown";
