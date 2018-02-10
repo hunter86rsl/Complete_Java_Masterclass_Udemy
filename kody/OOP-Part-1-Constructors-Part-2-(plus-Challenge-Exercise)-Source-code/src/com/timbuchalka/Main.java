e7d0853c5a536c01383d01c2983cfe7f2318c0b1
hunter86rsl <hunter86r@onet.eu>
2018-02-10 17:51:19 +0100

    lecture38

--- kody/OOP-Part-1-Constructors-Part-2-(plus-Challenge-Exercise)-Source-code/src/com/timbuchalka/Main.java
+++ kody/OOP-Part-1-Constructors-Part-2-(plus-Challenge-Exercise)-Source-code/src/com/timbuchalka/Main.java
@@ -8,15 +8,15 @@ public class Main {
         //
         // Create getters and setters for each field
         // Create two additional methods
-        // 1. To allow the customer to deposit funds (this should increment the balance field).
-        // 2. To allow the customer to withdraw funds. This should deduct from the balance field,
+        // 1.  To allow the customer to deposit funds (this should increment the balance field).
+        // 2.  To allow the customer to withdraw funds.  This should deduct from the balance field,
         // but not allow the withdrawal to complete if their are insufficient funds.
         // You will want to create various code in the Main class (the one created by IntelliJ) to
         // confirm your code is working.
         // Add some System.out.println's in the two methods above as well.
 
 //        Account bobsAccount = new Account(); // "12345", 0.00, "Bob Brown", "myemail@bob.com",
-        // "(087) 123-4567");
+//              //  "(087) 123-4567");
 //        System.out.println(bobsAccount.getNumber());
 //        System.out.println(bobsAccount.getBalance());
 //
@@ -49,5 +49,9 @@ public class Main {
         VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
         System.out.println(person3.getName());
         System.out.println(person3.getEmailAddress());
+
+
+
+
     }
 }
