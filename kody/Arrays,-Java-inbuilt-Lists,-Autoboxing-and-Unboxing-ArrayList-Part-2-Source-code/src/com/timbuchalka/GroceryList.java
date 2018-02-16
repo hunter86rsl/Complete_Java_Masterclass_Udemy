f62d0ab8a4a88124943121edd4d65b485c07b735
hunter86rsl <hunter86r@onet.eu>
2018-02-16 07:09:22 +0100

    lecture57

--- kody/Arrays,-Java-inbuilt-Lists,-Autoboxing-and-Unboxing-ArrayList-Part-2-Source-code/src/com/timbuchalka/GroceryList.java
+++ kody/Arrays,-Java-inbuilt-Lists,-Autoboxing-and-Unboxing-ArrayList-Part-2-Source-code/src/com/timbuchalka/GroceryList.java
@@ -1,5 +1,10 @@
+package com.timbuchalka;
+
 import java.util.ArrayList;
 
+/**
+ * Created by dev on 23/08/15.
+ */
 public class GroceryList {
     private ArrayList<String> groceryList = new ArrayList<String>();
 
@@ -9,18 +14,29 @@ public class GroceryList {
 
     public void printGroceryList() {
         System.out.println("You have " + groceryList.size() + " items in your grocery list");
-        for (int i = 0; i < groceryList.size(); i++) {
-            System.out.println((i + 1) + ". " + groceryList.get(i));
+        for(int i=0; i< groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
         }
     }
 
     public void modifyGroceryItem(int position, String newItem) {
         groceryList.set(position, newItem);
-        System.out.println("Grocery item " + (position + 1) + " has been modified.");
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
     }
 
     public void removeGroceryItem(int position) {
         String theItem = groceryList.get(position);
         groceryList.remove(position);
+    }
+
+    public String findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem);
+
+        int position = groceryList.indexOf(searchItem);
+        if(position >=0) {
+            return groceryList.get(position);
+        }
+
+        return null;
     }
 }
