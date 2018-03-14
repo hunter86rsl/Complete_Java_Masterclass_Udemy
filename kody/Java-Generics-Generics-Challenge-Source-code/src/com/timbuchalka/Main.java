72fa0d98c9466d223016a396f5e19fbad5f80731
hunter86rsl <hunter86@onet.eu>
2018-03-14 06:28:04 +0100

    lecture91

--- kody/Java-Generics-Generics-Challenge-Source-code/src/com/timbuchalka/Main.java
+++ kody/Java-Generics-Generics-Challenge-Source-code/src/com/timbuchalka/Main.java
@@ -3,6 +3,20 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
+	    // ArrayList<Team> teams;
+        // Collections.sort(teams);
+        // Create a generic class to implement a league table for a sport.
+        // The class should allow teams to be added to the list, and store
+        // a list of teams that belong to the league.
+        //
+        // Your class should have a method to print out the teams in order,
+        // with the team at the top of the league printed first.
+        //
+        // Only teams of the same type should be added to any particular
+        // instance of the league class - the program should fail to compile
+        // if an attempt is made to add an incompatible team.
+
+
         League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
         Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         Team<FootballPlayer> melbourne = new Team<>("Melbourne");
@@ -40,5 +54,26 @@ public class Main {
         reallyRaw.add(adelaideCrows);     // unchecked warning
         reallyRaw.add(baseballTeam);    // unchecked warning
         reallyRaw.add(rawTeam);         // unchecked warning
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
