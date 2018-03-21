d96a80b4154729cfab70b0ab588977fe5d4e6aaa
hunter86rsl <hunter86@onet.eu>
2018-03-21 06:27:20 +0100

    lecture105

--- kody/Java-Collections-Binary-Search-Source-code/src/com/timbuchalka/Main.java
+++ kody/Java-Collections-Binary-Search-Source-code/src/com/timbuchalka/Main.java
@@ -3,9 +3,9 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-        Theatre theatre = new Theatre("Olympian", 8, 12);
+	    Theatre theatre = new Theatre("Olympian", 8, 12);
 //        theatre.getSeats();
-        if(theatre.reserveSeat("H11")) {
+        if(theatre.reserveSeat("B13")) {
             System.out.println("Please pay");
         } else {
             System.out.println("Sorry, seat is taken");
