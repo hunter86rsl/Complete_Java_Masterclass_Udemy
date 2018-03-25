e3500cb65d8b5f9028d08c7d104b543ddbffde16
hunter86rsl <hunter86@onet.eu>
2018-03-25 18:45:46 +0200

    lecture115

--- kody/Java-Collections-HashSet-equals()-and-hashCode()-Source-code/src/com/timbuchalka/Main.java
+++ kody/Java-Collections-HashSet-equals()-and-hashCode()-Source-code/src/com/timbuchalka/Main.java
@@ -75,32 +75,38 @@ public class Main {
         planets.add(temp);
 
         System.out.println("Planets");
-        for (HeavenlyBody planet : planets) {
+        for(HeavenlyBody planet : planets) {
             System.out.println("\t" + planet.getName());
         }
 
-        HeavenlyBody body = solarSystem.get("Jupiter");
+        HeavenlyBody body = solarSystem.get("Mars");
         System.out.println("Moons of " + body.getName());
-        for (HeavenlyBody jupiterMoon : body.getSatellites()) {
+        for(HeavenlyBody jupiterMoon: body.getSatellites()) {
             System.out.println("\t" + jupiterMoon.getName());
         }
 
         Set<HeavenlyBody> moons = new HashSet<>();
-        for (HeavenlyBody planet : planets) {
+        for(HeavenlyBody planet : planets) {
             moons.addAll(planet.getSatellites());
         }
 
         System.out.println("All Moons");
-        for (HeavenlyBody moon : moons) {
+        for(HeavenlyBody moon : moons) {
             System.out.println("\t" + moon.getName());
         }
 
         HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
         planets.add(pluto);
 
-        for (HeavenlyBody planet : planets) {
+        for(HeavenlyBody planet : planets) {
             System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
         }
+
+        Object o = new Object();
+        o.equals(o);
+        "pluto".equals("");
+
+
 
     }
 }
