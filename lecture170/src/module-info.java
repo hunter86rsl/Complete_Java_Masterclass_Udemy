a1eb5893b7864def99fcff4a59a43b43d7a1f344
Adrian Nikiel <hunter86@onet.eu>
2019-01-20 12:22:09 +0100

    lecture170

---
+++ lecture170/src/module-info.java
@@ -0,0 +1,6 @@
+module lecture170 {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ lecture170/src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    public void onButtonClicked() {
+        System.out.println("Hello, " + nameField.getText() );
+    }
+}
